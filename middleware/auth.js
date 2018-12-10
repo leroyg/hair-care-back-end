275f6f0863dee0c55780c41ff039e397725bef9e
ryanboris <ryan.boris@gmail.com>
2018-12-10 07:26:10 -0500

    Creates the structure for the backend application by adding more middleware and templates for all aspects of MVP including server organization and routing, TDD unit and integration testing, models and data templates for db, and the starting templates for database authorization and authentication concerns.

---
+++ middleware/auth.js
@@ -0,0 +1,2 @@
+//require jsonwebtoken and config
+//TODO: write function that will evaluate the token and decode and send appropriate responses for its validity 
---
+++ routes/auth.js
@@ -0,0 +1,4 @@
+//TODO: validate req body  and find user in database
+//TODO: write function for validation of password 
+//TODO:  write function for token generation
+//TODO: write function for validation of schema using Joi package as additional validation of schema data
