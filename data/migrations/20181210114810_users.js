8471a9b495df74fbe3565729be460549c668fe9b
ryanboris <ryan.boris@gmail.com>
2018-12-10 12:50:17 -0500

    Migrations for registered users model created

--- data/migrations/20181210114810_users.js
+++ data/migrations/20181210114810_users.js
@@ -1,8 +1,13 @@
+exports.up = function(knex, Promise){
+	return knex.schema.createTable('users', table => {
+		table.increments()
 
-exports.up = function(knex, Promise) {
-  
-};
+		table.string('username').unique().notNullable()
+		table.string('password').notNullable()
+		table.boolean('isStylist')
+	})
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise){
+	return knex.schema.dropTableIfExists('users')
+}
