c8f35ac80081d7de99ba8ac2828de0683d430147
ryanboris <ryan.boris@gmail.com>
2018-12-10 08:07:33 -0500

    Fixes router issue and adds global middleware back in to directory

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const winston = require('winston')
 const express = require('express')
 const app = express()
+const router = express.Router()
+const logging = require('./startup/logging')
+const routes = require('./startup/routes')
 
-require('./startup/logging')()
-require('./startup/routes')(app)
-require('./startup/db')()
-require('./startup/config')()
-require('./startup/validation')()
+router.use(app)
+router.use(routes)
+router.use(logging)
 
 const port = process.env.PORT || 5000
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
