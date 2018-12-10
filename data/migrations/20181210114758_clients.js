5ed105bd93850225b55af7a216c3122fd6041dd0
ryanboris <ryan.boris@gmail.com>
2018-12-10 12:39:36 -0500

    Migrations created for clients model - note: clients are registered users that are not stylists

--- data/migrations/20181210114758_clients.js
+++ data/migrations/20181210114758_clients.js
@@ -1,8 +1,14 @@
+exports.up = function(knex, Promise){
+	return knex.schema.createTable('clients', table => {
+		table.increments()
 
-exports.up = function(knex, Promise) {
-  
-};
+		table.string('first_name').notNullable()
+		table.string('last_name').notNullable()
+		table.string('comment').references('id').inTable('stylists')
+		table.integer('rate_stylist').references('id').inTable('stylists').foreign('stylist_id')
+	})
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function(knex, Promise){
+	return knex.schema.dropTableIfExists('stylists')
+}
