4d1ca10dc8bd204597bd0255432b53c09f9bcb80
ryanboris <ryan.boris@gmail.com>
2018-12-10 12:34:53 -0500

    Migration model for table  established

---
+++ data/migrations/20181210114741_stylists.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('stylists', table => {
+      table.increments() 
+
+      table.string('first_name', 128).notNullable()
+      table.string('last_name', 128).notNullable()
+      table.string('phone_number', 10).notNullable().unique()
+      table.integer('rating').unsigned()
+      table.string('services_description')
+      table.string('specialty', 128)
+      table.integer('average_cost').unsigned().notNullable()
+      table.string('social_network_link', 128)
+      table.string('social_network_site', 128).references('social_network_link').inTable('stylists')
+      table.binary('profile_picture')
+      table.binary('portfolio_picture')
+
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('stylists')
+};
--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 const router = express.Router()
 const logging = require('./startup/logging')
 const routes = require('./startup/routes')
+const db = require('./startup/db.js')
 
 router.use(app)
 router.use(routes)
 router.use(logging)
+router.use(db)
 
 const port = process.env.PORT || 5000
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
