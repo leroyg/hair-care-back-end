92d3c284d2d0058a6f087fa66869db785d6d25a8
ryanboris <ryan.boris@gmail.com>
2018-12-11 12:59:37 -0500

    Fixes middleware

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
+require('dotenv').config()
 const morgan = require('morgan')
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
 const winston = require('./config/winston')
 const stylists = require('./routes/stylists.js')
-const globalMiddleWare = require('./middleware/globalMiddleware.js')
 
+const enableCors = function(req, res, next){
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+	res.header('Access-Control-Allow-Headers', 'Content-Type')
+	next()
+}
+
+const corsOptions = {
+	origin               : function(origin, callback){
+		if (origin === process.env.FRONTEND_URL || !origin) {
+			callback(null, true)
+		}
+		else {
+			callback(new Error('Not allowed by CORS'))
+		}
+	},
+	optionsSuccessStatus : 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const server = express()
+server.use(express.json())
 server.use(morgan('combined', { stream: winston.stream }))
-server.use(globalMiddleWare)
+server.use(enableCors)
+server.use(cors(corsOptions))
+server.use(helmet())
 
 server.use('/api/stylists', stylists)
 
