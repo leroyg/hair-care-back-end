27c15b822d3fd04ffdf9ad4713dc95aa70f8d25b
ryanboris <ryan.boris@gmail.com>
2018-12-11 14:16:17 -0500

    reseeds and corrects issue

--- data/seeds/clients.js
+++ data/seeds/clients.js
@@ -1,11 +1,11 @@
 exports.seed = function(knex, Promise){
 	// Deletes ALL existing entries
-	return knex('clients').truncate().then(function(){
+	return knex('clients').del().then(function(){
 		// Inserts seed entries
-		return knex('clients').insert(
-			{ id: 1, first_name: 'first', last_name: 'last', comment: 'you rock!', stylist_rating: '2' },
-			{ id: 2, first_name: 'first', last_name: 'last', comment: 'you rock!', stylist_rating: '2' },
-			{ id: 3, first_n2me: 'first', last_name: 'last', comment: 'you rock!', stylist_rating: '1' },
-		)
+		return knex('clients').insert([
+			{ id: 1, first_name: 'A', last_name: 'F', comment: 'you rock!', stylist_rating: '2' },
+			{ id: 2, first_name: 'B', last_name: 'E', comment: 'thanks!', stylist_rating: '2' },
+			{ id: 3, first_name: 'C', last_name: 'D', comment: 'best ever!', stylist_rating: '2' },
+		])
 	})
 }
