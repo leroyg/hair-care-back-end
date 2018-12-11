20bfe7ac347bee0cf4e71e09862f1d7eee7989d6
ryanboris <ryan.boris@gmail.com>
2018-12-11 12:37:49 -0500

    Fixes cors issue

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const morgan = require('morgan')
 const express = require('express')
 const winston = require('./config/winston')
+const helmet = require('helmet')
+const cors  = require('cors')
 const stylists = require('./routes/stylists.js')
 
-const server = express()
 
+const server = express()
+server.use(cors())
+server.use(helmet())
 server.use(morgan('combined', { stream: winston.stream }))
 
 server.use('/api/stylists', stylists)
