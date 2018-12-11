20125cdb476e2fc2bc9d0a8f3b02a0fafd2e00ac
ryanboris <ryan.boris@gmail.com>
2018-12-10 20:02:27 -0500

    Adds AUTH middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,2 +1,40 @@
-//require jsonwebtoken and config
-//TODO: write function that will evaluate the token and decode and send appropriate responses for its validity 
+const jwt = require('jsonwebtoken')
+
+const jwtKey = require('../_secrets/keys').jwtKey
+
+// quickly see what this file exports
+module.exports = {
+  authenticate,
+  generateToken,
+}
+
+// implementation details
+function authenticate(req, res, next){
+  const token = req.get('Authorization')
+
+  if (token) {
+    jwt.verify(token, jwtKey, (err, decoded) => {
+      if (err) return res.status(401).json(err)
+
+      req.decoded = decoded
+
+      next()
+    })
+  } else {
+    return res.status(401).json({
+      error : 'No token provided, must be set on the Authorization Header',
+    })
+  }
+}
+
+function generateToken(user){
+  const payload = {
+    userId   : user.id,
+    username : user.username,
+  }
+  const options = {
+    expiresIn : '1h',
+  }
+
+  return jwt.sign(payload, jwtKey, options)
+}
