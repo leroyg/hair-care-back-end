e8b96cb5d24baa45eec82618a16f7e360c0967a1
Carlos Vargas <cornielleandres@gmail.com>
2018-12-12 16:59:15 -0500

    Change table structures.

--- data/migrations/20181211165908_portfolio_pictures.js
+++ data/migrations/20181211165908_portfolio_pictures.js
@@ -5,7 +5,6 @@ exports.up = function(knex, Promise){
 
 		table.binary('portfolio_picture')
 		table.integer('stylist_id').notNullable().references('id').inTable('stylists').onDelete('CASCADE').index()
-		table.string('comment_id').notNullable().defaultTo('')
 		table.timestamps(true,true)
 	})
 }
--- data/migrations/20181211165909_comments.js
+++ data/migrations/20181211165909_comments.js
@@ -3,7 +3,6 @@ exports.up = function(knex, Promise){
 		table.increments().primary()
 
 		table.string('comment').notNullable().defaultTo('')
-		table.integer('stylist_id').notNullable().references('id').inTable('stylists').onDelete('CASCADE').index()
 		table.string('comment_by').notNullable().defaultTo('')
 		table.integer('picture_id').notNullable().references('id').inTable('portfolio_pictures').onDelete('CASCADE').index()
 		table.timestamps(true, true)
