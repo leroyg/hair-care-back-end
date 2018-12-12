61aa0c2797ec23d6d963c7b28ab47a1260f2e54d
ryanboris <ryan.boris@gmail.com>
2018-12-12 11:41:04 -0500

    minor fixes to gitignore and auth middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -14,7 +14,7 @@ function authenticate(req, res, next){
 	const token = req.get('Authorization')
 	if (token) {
 		jwt.verify(token, jwtKey, (err, decoded) => {
-			if (err) return res.status(401).json(err)
+			// if (err) return res.status(401).json(err)
 			if (err) return res.status(401).json({error: "Not authorized."})
 
 			req.decoded = decoded
