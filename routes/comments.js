21372e7bdd39a5937a43d28cd8e60d9fb0ed1cb8
ryanboris <ryan.boris@gmail.com>
2018-12-12 07:58:39 -0500

    Adds additional comment crud operations

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,11 @@ const database = require('../startup/db.js')
 const router = express.Router()
 const { authenticate } = require('../middleware/authentication.js')
 
-router.get('/', async (req, res) => {
+
+/**
+ * RESPONDS WITH AN ARRAY OF COMMENT OBJECTS
+ */
+router.get('/', authenticate, async (req, res) => {
 	try {
 		const data = await database('comments')
 		res.status(200).json(data)
@@ -15,7 +19,25 @@ router.get('/', async (req, res) => {
 	}
 })
 
-router.get('/:id', async (req, res) => {
+/**
+ * RESPONDS WITH ONE COMMENT OBJECT BASED ON COMMENT ID 
+ */
+
+router.get('/:id', authenticate, async (req, res) => {
+	try {
+		const byId = await database('comments').where('id', req.params.id)
+		if (!byId) return res.status(404).json({ message: 'ID not found' })
+		res.status(200).json(byId)
+	} catch (error) {
+		res.status(500).json({ error: 'An unexpected error has occuried.  Please try again.' })
+	}
+})
+
+/**
+ * RESPONDS WITH ALL THE COMMENT OBJECTS THAT HAVE THE SAME STYLIST_ID (ALL COMMENTS FOR A STYLIST) 
+ */
+
+router.get('/stylist/:id', async (req, res) => {
 	const { id } = req.params
 	try {
 		const clients = await database.select('*').from('comments').where('stylist_id', id)
@@ -26,7 +48,12 @@ router.get('/:id', async (req, res) => {
 	}
 })
 
-router.post('/stylists/:id', authenticate, async (req, res) => {
+/**
+ * RESPONDS WITH THE SUCCESSFUL POST BODY (CONTAINS COMMENT SCHEMA AND STYLIST ID(WHAT STYLIST IT BELONGS TO)
+ * AND ALSO CONTAINS WHICH USERNAME THE COMMENT WAS CREATED BY, WITH TIME CREATED AND TIME UPDATED TIMESTAMPS)
+ */
+
+router.post('/stylist/:id', authenticate, async (req, res) => {
 	const { id } = req.params
 	const { username } = req.decoded
 
@@ -37,7 +64,7 @@ router.post('/stylists/:id', authenticate, async (req, res) => {
 			const postIt = await database('comments').insert({
 				...req.body,
 				stylist_id : id,
-				comment_by: username
+				comment_by : username,
 			})
 			res.status(201).json({ id: postIt, comment_by: username, stylist_id: id })
 		} catch (error) {
@@ -46,6 +73,21 @@ router.post('/stylists/:id', authenticate, async (req, res) => {
 		}
 	} catch (error) {
 		console.log(error)
+		res.status(500).json({ error: 'An unexpected error has occuried.  Please try again.' })
+	}
+})
+
+/**
+ * THIS ROUTE DELETES ONE COMMENT BASED ON ITS COMMENT ID.  WHEN SUCCESSFUL IT RETURNS DELETEDID: 1 AND A SUCCESS MESSAGE.  IF ID NOT FOUND, IT RESPONDS WITH 404, ALL OTHER ERRORS RESPOND WITH 500.
+ */
+
+router.delete('/:id', authenticate, async (req, res) => {
+	const { id } = req.params
+	try {
+		const count = await database('comments').where('id', id).del()
+		if (count === 0) return res.status(404).json({ message: 'ID not found.' })
+		res.status(200).json({ count, deleted: 'The comment was deleted successfully.' })
+	} catch (error) {
 		res.status(500).json({ error: 'An unexpected error has occuried.  Please try again.' })
 	}
 })
