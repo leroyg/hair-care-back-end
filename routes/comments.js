967af57617256f894e70db99404a3370602c77ed
ryanboris <ryan.boris@gmail.com>
2018-12-12 08:19:02 -0500

    Adds in an update comment route

--- routes/comments.js
+++ routes/comments.js
@@ -2,7 +2,7 @@ const express = require('express')
 const database = require('../startup/db.js')
 const router = express.Router()
 const { authenticate } = require('../middleware/authentication.js')
-
+const moment = require('moment')
 
 /**
  * RESPONDS WITH AN ARRAY OF COMMENT OBJECTS
@@ -87,6 +87,33 @@ router.delete('/:id', authenticate, async (req, res) => {
 		const count = await database('comments').where('id', id).del()
 		if (count === 0) return res.status(404).json({ message: 'ID not found.' })
 		res.status(200).json({ count, deleted: 'The comment was deleted successfully.' })
+	} catch (error) {
+		res.status(500).json({ error: 'An unexpected error has occuried.  Please try again.' })
+	}
+})
+
+/**
+ * THIS ALLOWS ONE COMMENT TO BE UPDATED BY ID.  SUPPLY A JSON IN THE REQUEST BODY WITH "COMMENT" KEY
+ * SUCCESSFUL RESPONSE RETURNS 1:integer. COMMENT IS ALSO CHECKED BY USERNAME OF COMMENTED_BY IN ADDITION TO UNIQUE COMMENT ID--- ERRORS ARE HANDLED AS EXPECTED
+ */
+
+router.put('/:id', authenticate, async (req, res) => {
+	const { id } = req.params
+	const changes = req.body
+	const { username } = req.decoded.username
+	try {
+		const count = await database('comments').where('id', id)
+		if (count.length <= 0 && count.comment_by !== username)
+			return res.status(404).json({ message: 'ID/User not found.' })
+		try {
+			if (!req.body.comment) return res.status(400).json({ errorMessage: 'Please provide an updated comment.' })
+			const insert = await database('comments')
+				.where('id', id)
+				.update({ ...changes, updated_at: moment().format('YYYY-DD-MM HH:mm:ss') })
+			res.status(200).json({ insert, success: 'Success!' })
+		} catch (error) {
+			res.status(500).json({ error: 'An unexpected error has occuried.  Please try again.' })
+		}
 	} catch (error) {
 		res.status(500).json({ error: 'An unexpected error has occuried.  Please try again.' })
 	}
