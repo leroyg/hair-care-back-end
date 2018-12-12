74991b201118dc2c1181225004dd12d02c72e98e
ryanboris <ryan.boris@gmail.com>
2018-12-12 08:41:24 -0500

    migrates reviews model and seeds it

--- data/migrations/20181211202710_reviews.js
+++ data/migrations/20181211202710_reviews.js
@@ -1,12 +1,15 @@
 exports.up = function(knex, Promise){
 	return knex.schema.createTable('reviews', table => {
 		table.increments()
+			.primary()
 
-		table.string('review')
-		table.integer('stylist_id').references('id').inTable('stylists')
+		table.string('review').notNullable().defaultTo('')
+		table.integer('stylist_id').notNullable().references('id').inTable('stylists').onDelete('CASCADE').index()
+		table.string('review_by').notNullable().defaultTo('')
+		table.timestamps(true,true)
 	})
 }
 
 exports.down = function(knex, Promise){
-	return knex.schema.dropTableIfExists('reviews')
+	return knex.schema.dropTable('reviews')
 }
--- data/seeds/reviews.js
+++ data/seeds/reviews.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise){
 	return knex('reviews').del().then(function(){
 		// Inserts seed entries
 		return knex('reviews').insert([
-			{ id: 1, review: 'best work', stylist_id: 1 },
-			{ id: 2, review: 'awesome stylist', stylist_id: 2 },
-			{ id: 3, review: 'the best ever' , stylist_id: 1},
+			{ id: 1, review: 'good stuff', stylist_id: 1, review_by: 'Sum One' },
+			{ id: 2, review: 'good stuff', stylist_id: 1, review_by: 'Sum One' },
+			{ id: 3, review: 'good stuff', stylist_id: 1, review_by: 'Sum One' },
 		])
 	})
 }
