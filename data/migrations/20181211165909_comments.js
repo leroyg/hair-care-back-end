2f86a18c6a729cb210c46f8f0d6d8e9faa77de58
ryanboris <ryan.boris@gmail.com>
2018-12-12 03:06:30 -0500

    init on feature branch comments

--- data/migrations/20181211165909_comments.js
+++ data/migrations/20181211165909_comments.js
@@ -1,9 +1,11 @@
 exports.up = function(knex, Promise){
 	return knex.schema.createTable('comments', table => {
 		table.increments()
+			.primary()
 
-		table.string('comment')
-		table.integer('stylist_id').references('id').inTable('stylists')
+		table.string('comment').notNullable().defaultTo('')
+		table.integer('stylist_id').references('id').inTable('stylists').onDelete('CASCADE').index()
+		table.timestamps(true,true)
 	})
 }
 
