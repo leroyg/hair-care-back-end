3e99f4cb022c3f6dc09a5d198eb7961c9684ca0b
ryanboris <ryan.boris@gmail.com>
2018-12-12 18:13:12 -0500

    fixes seeds

--- data/seeds/comments.js
+++ data/seeds/comments.js
@@ -3,9 +3,9 @@ exports.seed = function(knex, Promise){
 	return knex('comments').del().then(function(){
 		// Inserts seed entries
 		return knex('comments').insert([
-			{ comment: 'you da best', stylist_id: 1, picture_id: 2 },
-			{ comment: 'cool', stylist_id: 2, picture_id: 1 },
-			{ comment: 'k', stylist_id: 1, picture_id: 2 },
+			{ comment: 'you da best', picture_id: 2 },
+			{ comment: 'cool', picture_id: 1 },
+			{ comment: 'k', picture_id: 3 },
 		])
 	})
 }
