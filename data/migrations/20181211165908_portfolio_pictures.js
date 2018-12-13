e0e82c77418c6efcc49f8bf01650a2ac529675b2
ryanboris <ryan.boris@gmail.com>
2018-12-13 13:58:19 -0500

    reseeds comments

--- data/migrations/20181211165908_portfolio_pictures.js
+++ data/migrations/20181211165908_portfolio_pictures.js
@@ -10,5 +10,5 @@ exports.up = function(knex, Promise){
 }
 
 exports.down = function(knex, Promise){
-	return knex.schema.dropTable('portfolio_pictures')
+	return knex.schema.dropTable('pictures')
 }
