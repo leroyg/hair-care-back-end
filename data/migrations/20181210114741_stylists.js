cfb3dd16faa745ca02a65cfdc840f83acf68bfff
ryanboris <ryan.boris@gmail.com>
2018-12-13 20:34:23 -0500

    fixes stylists model

--- data/migrations/20181210114741_stylists.js
+++ data/migrations/20181210114741_stylists.js
@@ -2,14 +2,14 @@ exports.up = function(knex, Promise){
 	return knex.schema.createTable('stylists', table => {
 		table.increments().primary()
 
-		table.string('first_name', 128).notNullable().defaultTo("")
-		table.string('last_name', 128).notNullable().defaultTo("")
+		table.string('first_name', 128).notNullable().defaultTo('')
+		table.string('last_name', 128).notNullable().defaultTo('')
 		table.string('address').notNullable()
-		table.string('city').notNullable().defaultTo("")
-		table.string('state').notNullable().defaultTo("")
+		table.string('city').notNullable().defaultTo('')
+		table.string('state').notNullable().defaultTo('')
 		table.integer('zip').unsigned().notNullable().defaultTo(5)
-		table.binary('profile_photo')
-		table.integer('user_id').notNullable().unique().references('id').inTable('users').onDelete('CASCADE').index()
+		table.binary('profile_photo').defaultTo('https://via.placeholder.com/300')
+		table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE').index()
 	})
 }
 
