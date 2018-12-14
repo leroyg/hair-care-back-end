4a350b85c6f99d1ef464463c46974ba162bec5e3
Carlos Vargas <cornielleandres@gmail.com>
2018-12-14 10:58:39 -0500

    Change seed.

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise){
 	// Deletes ALL existing entries
-	return knex('users').truncate().then(function(){
+	return knex('users').del().then(function(){
 		// Inserts seed entries
 		return knex('users').insert([
 			{ username: 'Alan', password: 'apass', isStylist: true },
