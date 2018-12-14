4843250814df02a741a9a028068b50e04ab423a0
ryanboris <ryan.boris@gmail.com>
2018-12-13 22:42:00 -0500

    sets likes to 0

--- data/migrations/20181211203901_likes.js
+++ data/migrations/20181211203901_likes.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise){
 	return knex.schema.createTable('likes', table => {
 		table.increments().primary()
 
-		table.integer('likes').unsigned().notNullable()
+		table.integer('likes').unsigned().notNullable().defaultTo(0)
 		table.integer('picture_id').notNullable().references('id').inTable('portfolio_pictures').onDelete('CASCADE').index()
 	})
 }
--- tests/api.spec.js
+++ tests/api.spec.js
@@ -1,4 +1,9 @@
 const request = require('supertest')
 const server = require('../routes/stylists.js')
 
-
+describe('sanity test', () => {
+	it('should respond with a status code of 200', async () => {
+		const response = await request(server).get('/api/stylists')
+		expect(response.status).toBe(200)
+	})
+})
